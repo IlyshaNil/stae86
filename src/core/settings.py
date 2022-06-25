8fbf5ebecb58d8a9a7b0ea8109e69733812a45fe
IlyshaNil <hacharoky86@gmail.com>
2022-06-25 23:59:32 +0300

    add: drf

--- src/core/settings.py
+++ src/core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'input.apps.InputConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
